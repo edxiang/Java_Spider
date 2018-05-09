55de6135878b7e0f9ea793aa9f4d7b3f6b0b3817
edxiao <229270808@qq.com>
2018-05-09 18:54:55 +0800

    download the whole page of Bootstrap3

--- src/main/java/org/kevin/Main/DownloadWebsite.java
+++ src/main/java/org/kevin/Main/DownloadWebsite.java
@@ -4,6 +4,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.kevin.Utils.DocUtils;
+import org.kevin.Utils.FileUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -11,40 +17,67 @@ import org.kevin.Utils.DocUtils;
  */
 public class DownloadWebsite {
     public static final String baseUrl = "https://v3.bootcss.com/";
+    private List<String> exist = new ArrayList<>();
 
     public static void main(String[] args) {
-        new DownloadWebsite().getSourceCodeAndLink("");
+        new DownloadWebsite().getSourceCodeAndLink(baseUrl, "index");
     }
 
-    private void getSourceCodeAndLink(String name) {
-        Document d = DocUtils.getDocument(baseUrl);
+    private void getSourceCodeAndLink(String url, String name) {
+        if (FileUtils.checkFileExist(name + ".html")) {
+            System.out.println("already exist.");
+            return;
+        }
+
+        Document d = DocUtils.getDocument(url);
         Elements linkTag = DocUtils.getElementsByDoc(d, "link");
         Elements scriptTag = DocUtils.getElementsByDoc(d, "script");
         Elements aTag = DocUtils.getElementsByDoc(d, "a");
-/*
-        System.out.println("here is the link:");
-        getHref(linkTag, "href");
-        System.out.println("here is the script:");
-        getHref(scriptTag, "src");*/
-        System.out.println("here is the a:");
-        getHref(aTag, "href");
+        Elements imgTag = DocUtils.getElementsByDoc(d, "img");
+
+        Map<String, String> maps = new HashMap<>();
+        getHref(linkTag, "href", "link", maps);
+        getHref(scriptTag, "src", "script", maps);
+        getHref(imgTag, "src", "img", maps);
+        getHref(aTag, "href", "a", maps);
+
+        FileUtils.downloadPage(name + ".html", url, maps);
     }
 
-    private void getHref(Elements elements, String mark) {
+    private void getHref(Elements elements, String mark, String tag, Map maps) {
         for (Element e : elements) {
             String href = e.attr(mark);
-            if (href.contains("#"))
+            String originHref = href;
+            if (href.contains("#") || href.contains("http"))
                 continue;
-            if (href.contains("http") && !mark.equals("src"))
-                continue;
-            System.out.println(e.outerHtml());
-            if(href.contains("../"))
-                href = href.replaceAll("\\.\\./","");
-            if(href.lastIndexOf("/") == href.length() - 1 && href.length() - 1 > 0){
-                href = href.substring(0,href.length() - 1) + ".html";
+            if (href.contains("../")) {
+                href = href.replaceAll("\\.\\./", "");
             }
-            System.out.println(" ---: " + href);
+            if (href.lastIndexOf("/") == href.length() - 1 && href.length() - 1 > 0) {
+                href = href.substring(0, href.length() - 1);
+            }
+            if (href.equals(""))
+                continue;
+
+            if (tag.equals("a")) {
+                if (exist.contains(href))
+                    return;
+                exist.add(href);
+                if (!href.equals(originHref))
+                    maps.put("=\"" + originHref, "=\"" + href + ".html");
+                getSourceCodeAndLink(baseUrl + href, href);
+            } else {
+                if (!href.equals(originHref))
+                    maps.put("=\"" + originHref, "=\"" + href);
+                if (exist.contains(href))
+                    return;
+                exist.add(href);
+                if (href.contains(".css") || href.contains(".js"))
+                    FileUtils.copyFromWebsite(href, baseUrl + href);
+                else
+                    FileUtils.downloadSource(href, baseUrl + href);
+
+            }
         }
-        System.out.println("____________");
     }
 }
--- src/main/java/org/kevin/Utils/FileUtils.java
+++ src/main/java/org/kevin/Utils/FileUtils.java
@@ -2,8 +2,11 @@ package org.kevin.Utils;
 
 import org.junit.Test;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Kevin.Z on 2017/11/19.
@@ -14,24 +17,33 @@ public class FileUtils {
     private static String albumName = ApplicationPropertiesUtils.getProperty("album.name");
     private static String fileConnect = "/";
 
-    public static File createFile(String title, String name){
-        try {
+    public static File createFile(String title, String name) {
+        return null;
+        /*try {
             File file = new File(basePath + fileConnect + albumName + fileConnect + title + fileConnect + name);
-            if(!file.exists()){
-                if(!file.getParentFile().exists()){
-                    if(file.getParentFile().mkdirs())
-                        if(file.createNewFile())
+            if (!file.exists()) {
+                if (!file.getParentFile().exists()) {
+                    if (file.getParentFile().mkdirs())
+                        if (file.createNewFile())
                             return file;
-                }else{
-                    if(file.createNewFile())
+                } else {
+                    if (file.createNewFile())
                         return file;
                 }
             }
-        } catch(Exception e){
+        } catch (Exception e) {
             LogUtils.setError("create file failed");
             e.printStackTrace();
         }
-        return null;
+        return null;*/
+    }
+
+    public static boolean checkFileExist(String relativePath) {
+        int split = relativePath.lastIndexOf("/") + 1;
+        String folders = relativePath.substring(0, split);
+        String name = relativePath.substring(split, relativePath.length());
+        File file = new File(basePath + fileConnect + folders + fileConnect + name);
+        return file.exists();
     }
 
     public static String getName(String s, String c) {
@@ -39,5 +51,123 @@ public class FileUtils {
             return s.substring(s.lastIndexOf(c) + 1);
         } else
             return null;
+    }
+
+    // works in css&js
+    public static File newFile(String relativePath) {
+        int split = relativePath.lastIndexOf("/") + 1;
+        String folders = relativePath.substring(0, split);
+        String name = relativePath.substring(split, relativePath.length());
+        try {
+            File file = new File(basePath + fileConnect + folders + fileConnect + name);
+            if (!file.exists()) {
+                if (!file.getParentFile().exists()) {
+                    if (file.getParentFile().mkdirs())
+                        if (file.createNewFile())
+                            return file;
+                } else {
+                    if (file.createNewFile())
+                        return file;
+                }
+            }
+        } catch (Exception e) {
+            LogUtils.setError("create file failed");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // works in css&js
+    public static void copyFromWebsite(String relativePath, String targetUrl) {
+        File targetFile = newFile(relativePath);
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try {
+            URL url = new URL(targetUrl);
+            URLConnection c = url.openConnection();
+            br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(targetFile)));
+            String s;
+            while ((s = br.readLine()) != null) {
+                bw.write(s + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+                if (bw != null)
+                    bw.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void downloadPage(String relativePath, String targetUrl, Map<String, String> maps) {
+        File targetFile = newFile(relativePath);
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try {
+            URL url = new URL(targetUrl);
+            URLConnection c = url.openConnection();
+            br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(targetFile)));
+            StringBuilder sb = new StringBuilder();
+            String s;
+            while ((s = br.readLine()) != null) {
+                /*bw.write(s + "\n");*/
+                sb.append(s + "\n");
+            }
+            s = sb.toString();
+            for (String key : maps.keySet()) {
+                if (s.contains(key)) {
+                    s = s.replaceAll(key, maps.get(key));
+                }
+            }
+            bw.write(s);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+                if (bw != null)
+                    bw.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void downloadSource(String relativePath, String targetUrl) {
+        File targetFile = newFile(relativePath);
+        if (targetFile == null)
+            return;
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        try {
+            URL url = new URL(targetUrl);
+            URLConnection c = url.openConnection();
+            bis = new BufferedInputStream(c.getInputStream());
+            bos = new BufferedOutputStream(new FileOutputStream(targetFile));
+            int length = -1;
+            byte[] bs = new byte[4 * 1024];
+            while ((length = bis.read(bs)) != -1) {
+                bos.write(bs, 0, length);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bis != null)
+                    bis.close();
+                if (bos != null)
+                    bos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
